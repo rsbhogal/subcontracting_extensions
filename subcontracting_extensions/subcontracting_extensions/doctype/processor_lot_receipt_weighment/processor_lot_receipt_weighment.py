0cc48c64a9bbb0405610d9cbd458231b9c433285
R S Bhogal <rsbhogal@gmail.com>
2026-08-30 12:55:49 +0530

    feat: add multi-item receipt schema

---
+++ subcontracting_extensions/subcontracting_extensions/doctype/processor_lot_receipt_weighment/processor_lot_receipt_weighment.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, R.S. Bhogal and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class ProcessorLotReceiptWeighment(Document):
+	pass
